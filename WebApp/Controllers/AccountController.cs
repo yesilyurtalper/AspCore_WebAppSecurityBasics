1925c57c0b0440f86d39993aa8dbcb89f719321f
yesilyurtalper <yesilyurtalper@gmail.com>
2022-10-22 23:44:33 +0300

    repo initialized

---
+++ WebApp/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebApp.Data.Account;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(SignInManager<User> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> ExternalLoginCallback()
+        {
+            var loginInfo = await signInManager.GetExternalLoginInfoAsync();
+            //var res = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var emailClaim = loginInfo.Principal.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email);
+            var userClaim = loginInfo.Principal.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name);
+
+            if (emailClaim != null && userClaim != null)
+            {
+                var user = new User { Email = emailClaim.Value, UserName = userClaim.Value };
+                await signInManager.SignInAsync(user, true);
+            }
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
